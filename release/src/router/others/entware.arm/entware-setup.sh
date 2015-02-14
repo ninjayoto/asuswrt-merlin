bfdf4c59eb8bd004bf571b25b58175ea970ca3af
Eric Sauvageau <rmerl@lostrealm.ca>
2015-02-13 23:50:02 -0500

    entware: Added ARM version of setup script, pointing to zyxmon's qnapware repository

--- release/src/router/others/entware.arm/entware-setup.sh
+++ release/src/router/others/entware.arm/entware-setup.sh
@@ -10,22 +10,24 @@ INPUT="$BOLD => $NORM"
 i=1 # Will count available partitions (+ 1)
 cd /tmp
 
-echo -e $INFO This script will guide you through the Entware installation.
-echo -e $INFO Script modifies only \"entware\" folder on the chosen drive,
+echo -e $INFO This script will guide you through the Entware-Arm installation.
+echo -e $INFO Script modifies only \"entware.arm\" folder on the chosen drive,
 echo -e $INFO no other data will be touched. Existing installation will be
 echo -e $INFO replaced with this one. Also some start scripts will be installed,
-echo -e $INFO the old ones will be saved to .\entware\jffs_scripts_backup.tgz
-echo 
+echo -e $INFO the old ones will be saved on partition where Entware is installed
+echo -e $INFO like /tmp/mnt/sda1/jffs_scripts_backup.tgz
+echo
 
 if [ ! -d /jffs/scripts ]
 then
-  echo -e "$ERROR Please enable JFFS partition from web UI, reboot router and"
-  echo -e "$ERROR try again.  Exiting..."
+  echo -e "$ERROR Please \"Enable JFFS partition\" & \"JFFS custom scripts and configs\""
+  echo -e "$ERROR from router web UI: www.asusrouter.com/Advanced_System_Content.asp"
+  echo -e "$ERROR then reboot router and try again. Exiting..."
   exit 1
 fi
 
 echo -e $INFO Looking for available  partitions...
-for mounted in `/bin/mount | grep -E 'ext2|ext3' | cut -d" " -f3`
+for mounted in `/bin/mount | grep -E 'ext2|ext3|ext4' | cut -d" " -f3`
 do
   isPartitionFound="true"
   echo "[$i] -->" $mounted
@@ -35,7 +37,7 @@ done
 
 if [ $i == "1" ]
 then
-  echo -e "$ERROR No ext2/ext3 partition available. Exiting..."
+  echo -e "$ERROR No ext2/ext3/ext4 partition available. Exiting..."
   exit 1
 fi
 
@@ -47,15 +49,15 @@ then
   exit 0
 fi
 
-if [ "$partitionNumber" -gt `expr $i - 1` ]                                                           
-then                                                                                       
-  echo -e "$ERROR Invalid partition number!  Exiting..."                                                                 
-  exit 1                                                                                   
+if [ "$partitionNumber" -gt `expr $i - 1` ]
+then
+  echo -e "$ERROR Invalid partition number!  Exiting..."
+  exit 1
 fi
 
 eval entPartition=\$mounts$partitionNumber
 echo -e "$INFO $entPartition selected.\n"
-entFolder=$entPartition/entware
+entFolder=$entPartition/entware.arm
 
 if [ -d $entFolder ]
 then
@@ -74,7 +76,7 @@ echo -e $INFO Creating /tmp/opt symlink...
 ln -s $entFolder /tmp/opt
 
 echo -e $INFO Creating /jffs scripts backup...
-tar -czf $entPartition/jffs_scripts_backup.tgz /jffs/scripts/* >/dev/nul
+tar -czf $entPartition/jffs_scripts_backup_`date +\%F_\%H-\%M`.tgz /jffs/scripts/* >/dev/nul
 
 echo -e "$INFO Modifying start scripts..."
 cat > /jffs/scripts/services-start << EOF
@@ -97,12 +99,13 @@ cat > /jffs/scripts/post-mount << EOF
 
 if [ \$1 = "__Partition__" ]
 then
-  ln -sf \$1/entware /tmp/opt
+  ln -sf \$1/entware.arm /tmp/opt
 fi
 EOF
 eval sed -i 's,__Partition__,$entPartition,g' /jffs/scripts/post-mount
 chmod +x /jffs/scripts/post-mount
 
-echo -e "$INFO Starting Entware deployment....\n"
-wget http://entware.wl500g.info/binaries/entware/installer/entware_install.sh
-sh ./entware_install.sh
+echo -e "$INFO Starting Entware-Arm deployment....\n"
+wget http://qnapware.zyxmon.org/binaries-armv7/installer/entware_install_arm.sh
+sh ./entware_install_arm.sh
+sync
