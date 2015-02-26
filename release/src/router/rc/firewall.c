86e68b649ab897c4b88792889e9f11a73b390e92
Eric Sauvageau <rmerl@lostrealm.ca>
2015-02-25 23:38:07 -0500

    firewall: Change NAT loopback mark value to avoid possibly trashing bit 0, used by QoS

--- release/src/router/rc/firewall.c
+++ release/src/router/rc/firewall.c
@@ -1271,7 +1271,7 @@ void nat_setting(char *wan_if, char *wan_ip, char *wanx_if, char *wanx_ip, char 
 			fprintf(fp, "-A POSTROUTING %s -o %s ! -s %s -j MASQUERADE\n", p, wanx_if, wanx_ip);
 
 		/* masquerade lan to lan */
-		fprintf(fp, "-A POSTROUTING %s -m mark --mark 0xd001 -j MASQUERADE\n" , p);
+		fprintf(fp, "-A POSTROUTING %s -m mark --mark 0xb400 -j MASQUERADE\n" , p);
 //		ip2class(lan_ip, nvram_safe_get("lan_netmask"), lan_class);
 //		fprintf(fp, "-A POSTROUTING %s -o %s -s %s -d %s -j MASQUERADE\n", p, lan_if, lan_class, lan_class);
 	}
@@ -1551,7 +1551,7 @@ void nat_setting2(char *lan_if, char *lan_ip, char *logaccept, char *logdrop)	//
 
 		// masquerade lan to lan
 
- 		fprintf(fp, "-A POSTROUTING %s -m mark --mark 0xd001 -j MASQUERADE\n", p);
+		fprintf(fp, "-A POSTROUTING %s -m mark --mark 0xb400 -j MASQUERADE\n", p);
 
 //		ip2class(lan_ip, nvram_safe_get("lan_netmask"), lan_class);
 //		fprintf(fp, "-A POSTROUTING %s -o %s -s %s -d %s -j MASQUERADE\n", p, lan_if, lan_class, lan_class);
@@ -4118,7 +4118,7 @@ mangle_setting(char *wan_if, char *wan_ip, char *lan_if, char *lan_ip, char *log
 
 /* For NAT loopback */
 	eval("iptables", "-t", "mangle", "-A", "PREROUTING", "!", "-i", wan_if,
-	     "-d", wan_ip, "-j", "MARK", "--set-mark", "0xd001");
+	     "-d", wan_ip, "-j", "MARK", "--set-mark", "0xb400");
 
 /* Workaround for incorrect DSCP from Comcast */
 	if (nvram_get_int("DSCP_fix_enable")) {
@@ -4255,7 +4255,7 @@ mangle_setting2(char *lan_if, char *lan_ip, char *logaccept, char *logdrop)
 		wan_if = get_wan_ifname(unit);
 
 		eval("iptables", "-t", "mangle", "-A", "PREROUTING", "!", "-i", wan_if,
-		     "-d", wan_ip, "-j", "MARK", "--set-mark", "0xd001");
+		     "-d", wan_ip, "-j", "MARK", "--set-mark", "0xb400");
 	}
 #endif
 
