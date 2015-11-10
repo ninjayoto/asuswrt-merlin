8ff2a10ae459182df689b7aca2d3f2c0ad3202cd
John Bacho <john.9527@hotmail.com>
2015-11-10 00:18:26 -0700

    watchdog: invoke reboot_schedule if ntp sync was ever successful

--- release/src/router/rc/watchdog.c
+++ release/src/router/rc/watchdog.c
@@ -912,7 +912,7 @@ void timecheck(void)
 	#ifdef RTCONFIG_REBOOT_SCHEDULE
 	/* Reboot Schedule */
 	char* reboot_schedule;
-	if (nvram_match("ntp_ready", "1") && nvram_match("reboot_schedule_enable", "1"))
+	if (nvram_match("ntp_sync", "1") && nvram_match("reboot_schedule_enable", "1"))
 	{
 		//SMTWTFSHHMM
 		//XXXXXXXXXXX
