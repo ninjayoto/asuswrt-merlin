f49c482da89585c8d620904826f7327bbf49c11f
John Bacho <john.9527@hotmail.com>
2017-01-05 03:18:59 -0700

    dnscrypt: add updater script

---
+++ release/src/router/others/dnscrypt-update-resolvers.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ "$1" == "default" ]; then
+	echo "Setting default list of public DNSCrypt resolvers..."
+	RESOLVERS_PATH="/rom"
+	RESOLVERS_FILE="${RESOLVERS_PATH}/dnscrypt-resolvers.csv"
+	rm -f "$(nvram get dnscrypt_csv)"
+else
+
+	RESOLVERS_PATH="/jffs/etc"
+	RESOLVERS_FILE="${RESOLVERS_PATH}/dnscrypt-resolvers.csv"
+	RESOLVERS_FILE_TMP="${RESOLVERS_FILE}.tmp"
+
+	RESOLVERS_URL="https://download.dnscrypt.org/dnscrypt-proxy/dnscrypt-resolvers.csv"
+	RESOLVERS_SIG_URL="${RESOLVERS_URL}.minisig"
+	RESOLVERS_SIG_PUBKEY="RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
+
+	mkdir -p "${RESOLVERS_PATH}"
+
+	echo "Updating the list of public DNSCrypt resolvers..."
+	curl -L -R "$RESOLVERS_URL" -o "$RESOLVERS_FILE_TMP" || (echo "Download failed" && exit 1)
+	if $(which minisign > /dev/null 2>&1); then
+	  curl -L -o "$RESOLVERS_FILE_TMP.minisig" "$RESOLVERS_SIG_URL" || (echo "Failed to retrieve minisig" && exit 1)
+	  minisign -V -P "$RESOLVERS_SIG_PUBKEY" -m "$RESOLVERS_FILE_TMP" || (echo "Signature verification failed" && exit 1)
+	  mv -f "${RESOLVERS_FILE_TMP}.minisig" "${RESOLVERS_FILE}.minisig"
+	fi
+	mv -f "$RESOLVERS_FILE_TMP" "$RESOLVERS_FILE"
+fi
+
+nvram set dnscrypt_csv="$RESOLVERS_FILE"
+nvram commit
+
+echo "Done"
