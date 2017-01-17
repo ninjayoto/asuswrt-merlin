fce97004f93383e46dd4e373a2423c45ca16bc05
John Bacho <john.9527@hotmail.com>
2017-01-17 12:32:56 -0700

    wanduck: add debug message for disconnect reason

--- release/src/router/rc/wanduck.c
+++ release/src/router/rc/wanduck.c
@@ -434,15 +434,21 @@ int detect_internet(int wan_unit){
 			)
 		link_internet = DISCONN;
 #ifndef NO_DETECT_INTERNET
-	else if(!get_packets_of_net_dev(wan_ifname, &rx_packets, &tx_packets) || rx_packets <= RX_THRESHOLD)
+	else if(!get_packets_of_net_dev(wan_ifname, &rx_packets, &tx_packets) || rx_packets <= RX_THRESHOLD) {
 		link_internet = DISCONN;
-	else if(!isFirstUse && (!do_dns_detect() && !do_tcp_dns_detect(wan_unit) && !do_ping_detect(wan_unit)))
+		csprintf("wanduck: disconnect by packet transfer check.\n");
+	}
+	else if(!isFirstUse && (!do_dns_detect() && !do_tcp_dns_detect(wan_unit) && !do_ping_detect(wan_unit))) {
 		link_internet = DISCONN;
+		csprintf("wanduck: disconnect by dns/ping check.\n");
+	}
 #endif
 #ifdef RTCONFIG_DUALWAN
 	else if((!strcmp(dualwan_mode, "fo") || !strcmp(dualwan_mode, "fb"))
-			&& wandog_enable == 1 && !isFirstUse && !do_ping_detect(wan_unit))
+			&& wandog_enable == 1 && !isFirstUse && !do_ping_detect(wan_unit)) {
 		link_internet = DISCONN;
+		csprintf("wanduck: dual wan (%d) disconnect by wandog.\n", wan_unit);
+	}
 #endif
 	else
 		link_internet = CONNED;
