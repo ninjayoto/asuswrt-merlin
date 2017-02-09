662eec24fce61c806b0b6d3912313a19b244f3be
John Bacho <john.9527@hotmail.com>
2017-02-09 12:32:08 -0700

    openvpn: increase wait time for time sync complete to accomodate dualwan

--- release/src/router/rc/openvpn.c
+++ release/src/router/rc/openvpn.c
@@ -1657,7 +1657,7 @@ void start_vpn_eas()
 
 	if (strlen(nvram_safe_get("vpn_serverx_eas")) == 0 && strlen(nvram_safe_get("vpn_clientx_eas")) == 0) return;
 	// wait for time sync for a while
-	i = 60;
+	i = 300;  // extend for dual wan environments
 	while (time(0) < 1325376000 && i--) {	//another way to check ntp_ready, system time < 01-Jan-2012
 		sleep(1);
 	}
