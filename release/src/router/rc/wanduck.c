9d52656fa7cece594364393208e9217893ec9f0e
John Bacho <john.9527@hotmail.com>
2017-03-11 21:52:44 -0700

    wanduck: always update wan uptime regardless of current wan unit

--- release/src/router/rc/wanduck.c
+++ release/src/router/rc/wanduck.c
@@ -1368,10 +1368,11 @@ void record_conn_status(int wan_unit){
 	}
 
 	// save new wan start time
-	if(conn_wanup && ((wan_unit == current_wan_unit)
-#ifdef RTCONFIG_DUALWAN
-		|| ((!strcmp(dualwan_mode, "lb") && (wan_unit == WAN_UNIT_FIRST))))
-#endif
+	if(conn_wanup 
+		// && ((wan_unit == current_wan_unit)
+//#ifdef RTCONFIG_DUALWAN
+//		|| ((!strcmp(dualwan_mode, "lb") && (wan_unit == WAN_UNIT_FIRST))))
+//#endif
 	){
 		timenow = (long int) (time(0));
 		wan_bootdelay = atol(nvram_get("wan_bootdly"));
