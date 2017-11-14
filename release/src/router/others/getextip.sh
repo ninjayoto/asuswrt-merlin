7816e2724d150c58b3c72960c8af17d2c1186272
John Bacho <john.9527@hotmail.com>
2017-11-13 23:17:42 -0700

    others: update getextip for gettunnelip syntax compatibility

--- release/src/router/others/getextip.sh
+++ release/src/router/others/getextip.sh
@@ -2,16 +2,29 @@
 # use STUN to find the external IP.
 
 DEV=$1
+max_instance=2
 servers="stun.l.google.com:19302 stun.stunprotocol.org stun.iptel.org default"
 
 if [ "$DEV" == "-h" ]
 then
 	echo "Get External IP Address via STUN"
 	echo "Usage: getextip.sh [interface]"
+	echo "Usage: gettunnelip.sh [vpn_instance]"
 	exit 0
 fi
 
 [ "$DEV" == "" ] && DEV=$(nvram get wan0_ifname)
+if [ ${#DEV} -eq 1 ] # instance specified
+then
+	if [ "${DEV}" == "0" ]; then DEV=$(nvram get wan0_ifname); else DEV="tun1${DEV}"; fi
+fi 
+INSTANCE="${DEV:$((${#DEV}-1)):1}"
+if [ "${DEV:0:3}" == "tun" -a $INSTANCE -gt $max_instance ]
+then
+	echo "Invalid client instance!"
+	exit 1
+fi
+
 for server in $servers; do
 	[ "$server" = "default" ] && server=
 	result=$(/usr/sbin/ministun -t 1000 -c 1 -i $DEV $server 2>/dev/null)
@@ -19,4 +32,5 @@ for server in $servers; do
 	result=""
 done
 
+[ "${DEV:0:3}" == "tun" ] && nvram set "vpn_client${INSTANCE}_rip"=$result
 echo "$result"
