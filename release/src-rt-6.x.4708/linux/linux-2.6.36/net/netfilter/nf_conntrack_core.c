3fd6a517a896c45f95dbc13f85636d8459c8fbc3
John Bacho <john.9527@hotmail.com>
2018-03-24 15:26:35 -0700

    Revert "ARM: conntrack changes from 384_10007"
    This reverts commit f93224c7ea6ca7b21c27149ac82d79aba24b2970.

--- release/src-rt-6.x.4708/linux/linux-2.6.36/net/netfilter/nf_conntrack_core.c
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/net/netfilter/nf_conntrack_core.c
@@ -131,7 +131,7 @@ ip_conntrack_is_ipc_allowed(struct sk_buff *skb, u_int32_t hooknum)
 		/* Add ipc entry if packet is received on ctf enabled interface
 		 * and the packet is not a defrag'd one.
 		 */
-		if (ctf_isenabled(kcih, dev))
+		if (ctf_isenabled(kcih, dev) && (skb->len <= dev->mtu))
 			skb->nfcache |= NFC_CTF_ENABLED;
 	}
 
@@ -251,9 +251,6 @@ ip_conntrack_ipct_add(struct sk_buff *skb, u_int32_t hooknum,
 		if (ct->proto.tcp.state >= TCP_CONNTRACK_FIN_WAIT &&
 			ct->proto.tcp.state <= TCP_CONNTRACK_TIME_WAIT)
 			return;
-
-		if (skb->len > skb->dev->mtu)
-			return;
 	}
 	else if (protocol != IPPROTO_UDP)
 		return;
@@ -348,8 +345,17 @@ ip_conntrack_ipct_add(struct sk_buff *skb, u_int32_t hooknum,
 #endif /* CONFIG_IPV6 */
 	}
 	ipc_entry.tuple.proto = protocol;
-	ipc_entry.tuple.sp = tcph->source;
-	ipc_entry.tuple.dp = tcph->dest;
+#if defined(CONFIG_IPV6) && 0 /* breaks ICMP error forward */
+	if (ipver == 6 && protocol == IPPROTO_UDP) {
+		ipc_entry.tuple.sp = FRAG_IPV6_UDP_DUMMY_PORT;
+		ipc_entry.tuple.dp = FRAG_IPV6_UDP_DUMMY_PORT;
+	}
+	else
+#endif
+	{
+		ipc_entry.tuple.sp = tcph->source;
+		ipc_entry.tuple.dp = tcph->dest;
+	}
 
 	ipc_entry.next = NULL;
 
@@ -505,12 +511,10 @@ PX_PROTO_PPPOE:
 		ipc_entry.action |= CTF_ACTION_PPPOE_ADD;
 		skb->ctf_pppoe_cb[0] = 2;
 		ipc_entry.ppp_ifp = skb_dst(skb)->dev;
-		ipc_entry.tuple.sid = 0;
 	} else if ((skb->dev->flags & IFF_POINTOPOINT) && (skb->ctf_pppoe_cb[0] == 1)) {
 		ipc_entry.action |= CTF_ACTION_PPPOE_DEL;
 		ipc_entry.pppoe_sid = *(uint16 *)&skb->ctf_pppoe_cb[2];
 		ipc_entry.ppp_ifp = skb->dev;
-		ipc_entry.tuple.sid = ipc_entry.pppoe_sid;
 	}
 #endif
 
@@ -620,9 +624,6 @@ PX_PROTO_PPTP_L2TP:
 			ipc_entry.dhost.octet[2], ipc_entry.dhost.octet[3],
 			ipc_entry.dhost.octet[4], ipc_entry.dhost.octet[5]);
 	printk("[%d] vid: %d action %x\n", hooknum, ipc_entry.vid, ipc_entry.action);
-#ifdef CTF_PPPOE
-	printk("[%d] sid: 0x%4.4x\n", hooknum, ipc_entry.tuple.sid);
-#endif
 	if (manip != NULL)
 		printk("manip_ip: %u.%u.%u.%u manip_port %u\n",
 			NIPQUAD(ipc_entry.nat.ip), ntohs(ipc_entry.nat.port));
@@ -850,15 +851,18 @@ ip_conntrack_ipct_resume(struct sk_buff *skb, u_int32_t hooknum,
 		tuple.family = AF_INET6;
 #endif /* CONFIG_IPV6 */
 	}
-	tuple.src_port = tcph->source;
-	tuple.dst_port = tcph->dest;
-	tuple.protocol = protocol;
-
-#ifdef CTF_PPPOE
-	if ((skb->dev->flags & IFF_POINTOPOINT) && (skb->ctf_pppoe_cb[0] == 1)) {
-		tuple.sid = *(uint16 *)&skb->ctf_pppoe_cb[2];
+#if defined(CONFIG_IPV6) && 0 /* breaks ICMP error forward */
+	if (ipver == 6 && protocol == IPPROTO_UDP) {
+		tuple.src_port = FRAG_IPV6_UDP_DUMMY_PORT;
+		tuple.dst_port = FRAG_IPV6_UDP_DUMMY_PORT;
 	}
+	else
 #endif
+	{
+		tuple.src_port = tcph->source;
+		tuple.dst_port = tcph->dest;
+	}
+	tuple.protocol = protocol;
 
 #ifdef CONFIG_NF_CONNTRACK_MARK
 	if (ct->mark != 0) {
@@ -1034,6 +1038,14 @@ destroy_conntrack(struct nf_conntrack *nfct)
 	 * before connection is in the list, so we need to clean here,
 	 * too. */
 	nf_ct_remove_expectations(ct);
+
+	#if defined(CONFIG_NETFILTER_XT_MATCH_LAYER7) || defined(CONFIG_NETFILTER_XT_MATCH_LAYER7_MODULE)
+	if(ct->layer7.app_proto)
+		kfree(ct->layer7.app_proto);
+	if(ct->layer7.app_data)
+	kfree(ct->layer7.app_data);
+	#endif
+
 
 	/* We overload first tuple to link into unconfirmed list. */
 	if (!nf_ct_is_confirmed(ct)) {
