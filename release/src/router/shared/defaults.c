8f348d3a1e8749cfdf7f312863230f8c3101f6de
John Bacho <john.9527@hotmail.com>
2018-08-02 04:13:43 -0700

    stubby: add nvram only option for stubby_noipv6

--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -760,6 +760,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "stubby_dns", ""},			// server list
 	{ "stubby_ipv4", "0"},	 		// number of ipv4 servers defined
 	{ "stubby_ipv6", "0"},	 		// number of ipv6 servers defined
+	{ "stubby_noipv6", "0"},		// do not configure ipv6 servers
 	{ "stubby_log", "5"},			// log level
 	{ "stubby_nologs", "0"},		// Show only non-logging servers
 	{ "stubby_csv","/rom/stubby-resolvers.csv"},	// list of DoT enabled resolvers
