86710689c2425a396cff3ba870ed7d858172b205
John Bacho <john.9527@hotmail.com>
2018-11-16 22:14:54 -0700

    samba: add min protocal statement for SMB1+SMB2 option
    fixes Win10 unable to access router shares via SMB1

--- release/src/router/libdisk/write_smb_conf.c
+++ release/src/router/libdisk/write_smb_conf.c
@@ -281,12 +281,14 @@ int main(int argc, char *argv[])
 //	fprintf(fp, "null passwords = yes\n");		// ASUS add (remove deprecated)
 #if defined(RTCONFIG_SAMBA36X) || defined(RTCONFIG_SAMBA_MODERN)
 	// 0 - smb1, 1 = smb2, 2 = smb1 + smb2
+	if (nvram_get_int("smbd_protocol") == 2)
+		fprintf(fp, "min protocol = NT1\n");
+	if (nvram_get_int("smbd_protocol") == 1)
+		fprintf(fp, "min protocol = SMB2\n");
 	if (nvram_get_int("smbd_protocol") == 0)
 		fprintf(fp, "max protocol = NT1\n");
 	else
 		fprintf(fp, "max protocol = SMB2\n");
-	if (nvram_get_int("smbd_protocol") == 1)
-		fprintf(fp, "min protocol = SMB2\n");
 
 	fprintf(fp, "passdb backend = smbpasswd\n");
 	fprintf(fp, "smb encrypt = disabled\n");
